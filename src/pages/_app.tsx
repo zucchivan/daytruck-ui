73192afa0b1e3486f37085b6b0c709a1a5bd6ff9
Ivan Z. dos Santos <zucchivan@gmail.com>
2023-03-25 11:13:55 +0000

    Adds Auth/OAuth through Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
@@ -11,9 +11,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
